3a569a3c629ec6a0d91f669f532ae93451131261
Rana Usman <usman4243ch@gmail.com>
2024-03-28 07:37:03 -0700

    New Changes

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const user = require("./routes/userRoute.js")
 app.use('/api/v1', user);
 
-app.get('/', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")))
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-})
+// app.get('/', (req, res) => {
+//     app.use(express.static(path.resolve(__dirname, "frontend", "build")))
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+// })
 
 // Error handling middleware - register only once
 app.use(errorMiddleware);
